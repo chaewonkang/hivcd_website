8d0676263f6ddef659098567b0cb74a93dbf8a53
seogyugim <asap0208@gmail.com>
2020-12-01 21:25:55 +0900

    --no-edit

--- backend/apps/postings/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post, Photo, Comment, File
-
-
-admin.site.register(Post)
-admin.site.register(Photo)
-admin.site.register(Comment)
-admin.site.register(File)
