4f3bfd00d588991a4c8adf598123367684bdef3f
seogyugim <asap0208@gmail.com>
2020-12-03 01:26:18 +0900

    --no-edit

--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["hongiksidi-env.eba-2gniavxu.us-west-2.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["hivcd-env.eba-xbhnr3bs.ap-northeast-2.elasticbeanstalk.com"]
 
 DATABASES = {
     "default": {
