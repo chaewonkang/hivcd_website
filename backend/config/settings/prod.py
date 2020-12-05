a5d2055702ee3bb1637abacb684a66175af7f568
seogyugim <asap0208@gmail.com>
2020-12-06 02:11:43 +0900

    --no-edit

--- backend/config/settings/prod.py
+++ backend/config/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://hivcdapp.azurewebsites.net/"]
 
 DATABASES = {
     "default": {
