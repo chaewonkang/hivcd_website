67eae87cd7d3008a9b849af7618d8290e9ae80c5
Chaewon Kang <ilpermet@gmail.com>
2020-12-07 18:37:55 +0900

    deleting backend for merging with latest azure-deploy branch

--- backend/apps/api_v1/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-
-
-app_name = "api_v1"
-
-urlpatterns = [
-    path("auth/", include("apps.authentications.urls", namespace="auth")),
-    path("postings/", include("apps.postings.urls", namespace="postings")),
-]
