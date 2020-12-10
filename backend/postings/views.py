9e0d07af3ea9be2c74c1b24bdb19aee15799e5f2
seogyugim <asap0208@gmail.com>
2020-12-10 17:07:27 +0900

    --no-edit

--- backend/postings/views.py
+++ backend/postings/views.py
@@ -1,19 +1,25 @@
 from django.conf import settings
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
 
-class PostViewset(viewsets.ModelViewSet):
+class PostListAPIView(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
-class CommentsViewset(viewsets.ModelViewSet):
-    serializer_class = CommentSerializer
-    queryset = Comment.objects.all()
+class PostRetrieveAPIView(generics.RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class CommentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
     permission_classes = (IsAuthenticatedOrReadOnly,)
