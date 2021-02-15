a1800f2d64a3a6ed3c0f3ac6bf3c7373238b7c42
Chaewon Kang <ilpermet@gmail.com>
2021-02-15 15:52:31 +0900

    login test

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { ResponsiveHeader } from "../../components";
 import "./Header.css";
 import { Login, Search } from "../../components";
+import axios from "axios";
 
 function Header({ handleLogin, handleLogout, handleSearchKeyword }) {
   return (
@@ -42,7 +43,7 @@ function Header({ handleLogin, handleLogout, handleSearchKeyword }) {
           </div>
           <div className="header_container_system">
             <div className="header_container_login">
-              <a href="http://www.hongik.ac.kr/login.do?Refer=http://devsidi.hongik.ac.kr">
+              <a href="http://hongik.ac.kr/login.do?Refer=http://devsidi.hongik.ac.kr/api/v1/auth/login">
                 <Login
                   handleLogout={handleLogout}
                   handleLogin={handleLogin}
