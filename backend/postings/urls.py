9156e7c1526b9745b9fd98265e4fb95c72cd7577
seogyugim <asap0208@gmail.com>
2021-02-15 22:58:17 +0900

    post list/detail, comment list/create

--- backend/postings/urls.py
+++ backend/postings/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = "postings"
 
 urlpatterns = [
-    # path("test", views.post_list),
+    path("test/list/", views.post_list),
+    path("test/detail/", views.post_detail),
+    path("test/comments/", views.post_comment),
     path("", views.PostListAPIView.as_view()),
     path("<int:pk>/", views.PostRetrieveAPIView.as_view()),
     path("<int:pk>/comments/", views.CommentListCreateAPIView.as_view()),
