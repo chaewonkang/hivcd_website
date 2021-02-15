594d2f76c836ff284368f4b0fec23e9cfbd18304
seogyugim <asap0208@gmail.com>
2021-02-15 13:13:20 +0900

    로그인 만들기

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 
 class Account(models.Model):
-    snumber = models.CharField(max_length=10, default="", blank=False)
+    userid = models.CharField(max_length=10, default="", blank=False)
