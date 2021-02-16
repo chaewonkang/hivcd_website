1160398360076d8b156efeae89162377cb7fd5cb
seogyugim <asap0208@gmail.com>
2021-02-16 12:03:32 +0900

    --no-edit

--- backend/postings/urls.py
+++ backend/postings/urls.py
@@ -4,9 +4,6 @@ from . import views
 app_name = "postings"
 
 urlpatterns = [
-    path("test/list/", views.post_list),
-    path("test/detail/", views.post_detail),
-    path("test/comments/", views.post_comment),
     path("", views.PostListAPIView.as_view()),
     path("<int:pk>/", views.PostRetrieveAPIView.as_view()),
     path("<int:pk>/comments/", views.CommentListCreateAPIView.as_view()),
