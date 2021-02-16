7c4880b135664db3d99e7571f447827864414112
Chaewon Kang <ilpermet@gmail.com>
2021-02-16 18:35:11 +0900

    modified Eachpost File Error

--- frontend/src/components/EachPost/EachPost.js
+++ frontend/src/components/EachPost/EachPost.js
@@ -116,12 +116,12 @@ function EachPost({ postId, handleNavigateClick }) {
         <hr></hr>
         <div className="each_post_files">
           <span className="attached_file">
-            첨부파일 {eachPost.files[0].name}
+            첨부파일 {eachPost.files[0] ? eachPost.files[0].name : 없음}
           </span>
           <a
-            href={eachPost.files[0].files}
+            href={eachPost.files[0] ? eachPost.files[0].name : ""}
             target="_blank"
-            download={eachPost.files[0].files}
+            download={eachPost.files[0] ? eachPost.files[0].name : ""}
             rel="noopener noreferrer"
           >
             <button className="download_button">다운로드</button>
