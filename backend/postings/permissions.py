3964a83bc52ab153245e6dd1f364c6bba7544eba
seogyugim <asap0208@gmail.com>
2021-02-19 23:20:26 +0900

    --no-edit

--- backend/postings/permissions.py
+++ backend/postings/permissions.py
@@ -3,22 +3,21 @@ from rest_framework.permissions import BasePermission
 from auth.models import Account
 from auth.decrypt import decrypt
 
+
 class CookiePermission(BasePermission):
     """
     시각디자인학과에 관련된 사람인지 판단해서 권한 여부 판단
     """
 
     def has_permission(self, request, view):
-        cookie_id = request.COOKIES['SUSER_ID']
+        cookie_id = request.COOKIES["SUSER_ID"]
         key = os.environ.get("AUTH_KEY")
         s_id = decrypt(s=cookie_id, key=key)
+        s_id = s_id[:7]
 
         try:
             account = Account.objects.get(suser_id=s_id).cache()
         except:
             return False
 
-        if account.is_sidi == True:
-            return True
-        else:
-            return False
+        return account.is_sidi == True
