b98ceb4a16da5cec7386c33dd48fcf0c290dedd7
seogyugim <asap0208@gmail.com>
2021-02-19 23:03:00 +0900

    --no-edit

--- backend/postings/permissions.py
+++ backend/postings/permissions.py
@@ -1,6 +1,7 @@
+import os
 from rest_framework.permissions import BasePermission
 from auth.models import Account
-
+from auth.decrypt import decrypt
 
 class CookiePermission(BasePermission):
     """
@@ -9,6 +10,9 @@ class CookiePermission(BasePermission):
 
     def has_permission(self, request, view):
         cookies = request.COOKIES
-        user = request.user
-
-        return Account.objects.get(pk=user.pk).is_sidi == True
+        try:
+            sid = decrypt(s=cookies['SUSER_ID'], key=os.getenv("AUTH_KEY"))
+            account = Account.objects.get(suser_id=sid):
+        except:
+            return False
+        return account.is_sidi == True
