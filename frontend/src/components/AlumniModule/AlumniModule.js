6ca9a7fc1e683bb1e2cf17c2cc465a0a13f2025a
Chaewon Kang <ilpermet@gmail.com>
2021-02-22 14:06:08 +0900

    m

--- frontend/src/components/AlumniModule/AlumniModule.js
+++ frontend/src/components/AlumniModule/AlumniModule.js
@@ -1,44 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./AlumniModule.css";
 
-const AlumniModule = ({ key, title, id, body, category, thumbnailUrl }) => {
-  let categoryName = null;
-  if (category === 5) categoryName = "졸업 주간";
-  else if (category === 6) categoryName = "와우영상제";
-  else if (category === 7) categoryName = "기타";
-  const postId = parseInt(id, 10);
+const AlumniModule = ({ year, name, tel, url }) => {
   return (
-    <Link to={`/exhibition/${id}`}>
-      <div className="archive">
-        <div className="archive_tag">
-          <span>{categoryName}</span>
-        </div>
-        <div className="archive_content">
-          <div className="archive_content_header">
-            <span>
-              {title} <br></br>
-            </span>
-            <p className="archive_content_body">
-              {body.split("\n").map((line) => {
-                return (
-                  <span>
-                    {line}
-                    <br />
-                  </span>
-                );
-              })}
-            </p>
-          </div>
-        </div>
-        <div
-          className="archive_content_image image_fill"
-          style={{
-            backgroundImage: `url(${thumbnailUrl})`,
-          }}
-        ></div>
-      </div>
-    </Link>
+    <div className="alumni_module_container">
+      {year}
+      <br></br>
+      {name}
+      <br></br>
+      {tel}
+      <br></br>
+      {url}
+    </div>
   );
 };
 
--- frontend/src/components/ArchiveModule/ArchiveModule.js
+++ frontend/src/components/ArchiveModule/ArchiveModule.js
@@ -1,32 +1,44 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./ArchiveModule.css";
 
-const ArchiveModule = ({ title, thumbnailUrl, date, category, link }) => {
+const ArchiveModule = ({ title, thumbnailUrl, category, id, body, link }) => {
   let categoryName = null;
   if (category === 5) categoryName = "졸업 주간";
   else if (category === 6) categoryName = "와우영상제";
   else if (category === 7) categoryName = "기타";
-  const slicedDate = date.slice(0, 10);
+  const postId = parseInt(id, 10);
   return (
-    <div className="archive_module">
-      <div className="archive_module_tag">
-        <span>{categoryName}</span>
-      </div>
-      <div className="archive_module_content">
-        <div className="archive_module_content_header">
-          <a href={link} target="_blank" rel="noopener noreferrer">
-            {title} <br></br>
-          </a>
-          {slicedDate}
+    <Link to={`/exhibition/${postId}`}>
+      <div className="archive">
+        <div className="archive_tag">
+          <span>{categoryName}</span>
         </div>
+        <div className="archive_content">
+          <div className="archive_content_header">
+            <span>
+              {title} <br></br>
+            </span>
+            <p className="archive_content_body">
+              {body.split("\n").map((line) => {
+                return (
+                  <span>
+                    {line}
+                    <br />
+                  </span>
+                );
+              })}
+            </p>
+          </div>
+        </div>
+        <div
+          className="archive_content_image image_fill"
+          style={{
+            backgroundImage: `url(${thumbnailUrl})`,
+          }}
+        ></div>
       </div>
-      <div
-        className="archive_module_content_image image_fill"
-        style={{
-          backgroundImage: `url(${thumbnailUrl})`,
-        }}
-      ></div>
-    </div>
+    </Link>
   );
 };
 
