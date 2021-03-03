90e6a0e4a878a49e979ea9f66601439d4e2f8015
Chaewon Kang <ilpermet@gmail.com>
2021-03-03 12:38:22 +0900

    [BACKEND] modified base url

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -123,8 +123,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "build", "static"),
-    os.path.join(BASE_DIR, 'assets/static'),
-    os.path.join(BASE_DIR, "assets"),
 ]
 
 # Media files
