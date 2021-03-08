6801566b76d73c25aacf4ebd487d76b932aecf80
Chaewon Kang <ilpermet@gmail.com>
2021-03-08 16:50:59 +0900

    [TEST] hidden overflowed components

--- frontend/src/components/ArchiveWrapper/ArchiveWrapper.js
+++ frontend/src/components/ArchiveWrapper/ArchiveWrapper.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./ArchiveWrapper.css";
 
-const ArchiveWrapper = ({ children }) => {
-  return <div className="archivewrapper">{children}</div>;
+const ArchiveWrapper = ({ children, dimensions }) => {
+  return (
+    <div
+      className="archivewrapper"
+      style={{
+        width: dimensions.width * 0.4 - 30 + "px",
+        height: dimensions.height - 50 + "px",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default ArchiveWrapper;
