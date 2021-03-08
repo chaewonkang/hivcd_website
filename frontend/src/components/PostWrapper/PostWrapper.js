1ce86443f38cf4c12967195634e973665f7dc0a4
Chaewon Kang <ilpermet@gmail.com>
2021-03-08 15:55:44 +0900

    [TEST] grid style for each module

--- frontend/src/components/PostWrapper/PostWrapper.js
+++ frontend/src/components/PostWrapper/PostWrapper.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./PostWrapper.css";
 
 const PostWrapper = ({ children, dimensions }) => {
+	const [widthAndHeight, setWidthAndHeight] = useState({
+		width: dimensions.width * 0.6;
+		height: dimensions.height
+	})
   return (
     <div
       className="postwrapper"
-      style={{ width: dimensions.width, height: dimensions.height }}
+      style={{ width: widthAndHeight.width, height: widthAndHeight.height }}
     >
       {children}
     </div>
