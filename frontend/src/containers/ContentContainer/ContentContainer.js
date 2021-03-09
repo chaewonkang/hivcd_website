4f54f24a1e3c55238c76ed586e4f3afd508932e2
Chaewon Kang <ilpermet@gmail.com>
2021-03-09 18:14:15 +0900

    m

--- frontend/src/containers/ContentContainer/ContentContainer.js
+++ frontend/src/containers/ContentContainer/ContentContainer.js
@@ -51,6 +51,11 @@ function ContentContainer() {
   const HAS_VISITED_BEFORE = localStorage.getItem("hasVisitedBefore");
 
   useEffect(() => {
+    if (posts) {
+      const count = Object.keys(posts).length;
+      setPostCount(count);
+      console.log(postCount);
+    }
     const handleShowModal = () => {
       if (HAS_VISITED_BEFORE && HAS_VISITED_BEFORE > new Date()) {
         return;
@@ -97,93 +102,88 @@ function ContentContainer() {
     );
 
   if (!posts) return null;
-  if (posts) {
-    const count = Object.keys(posts).length;
-    setPostCount(count);
-    console.log(postCount);
-    return (
-      <div className="contentcontainer">
-        {showModal && (
-          <Modal
-            visible={showModal}
-            width="fit-content"
-            height="fit-content"
-            effect="fadeInDown"
-            onClickAway={() => handleClose()}
-          >
-            <div className="welcome_modal">
-              <p className="paragraph">
-                홍익시디 웹사이트가 오픈되었습니다.{" "}
-                <span role="img" aria-label="emoji">
-                  🎊
-                </span>
-                <br></br>
-                <br></br>
-                상단 메뉴바의 로그인 버튼을 통해 학번으로 로그인해 주세요.
-                로그인 후 학과사무실에서 승인이 완료되면 전체 게시물을 열람
-                가능합니다.
-                <br></br>
-                모바일 브라우저에서 로그아웃이 제대로 안될 경우에는 브라우저
-                쿠키를 삭제 후 재시도 해 주세요.<br></br>
-                <br></br>
-                기타 다른 문제가 있을 경우에는 yinyang.fig@gmail.com으로 연락
-                바랍니다.
-              </p>
-            </div>
-          </Modal>
-        )}
-        <PostWrapper dimensions={dimensions}>
-          {posts &&
-            posts
-              .filter(
-                (data) =>
-                  data.category === 1 ||
-                  data.category === 2 ||
-                  data.category === 3 ||
-                  data.category === 4
-              )
-              .map((post) => (
-                <>
-                  <LogoImage postCount={postCount}></LogoImage>
-                  <Post
-                    key={post.pk}
-                    title={post.title}
-                    date={post.updated}
-                    category={post.category}
-                    id={post.pk}
-                  ></Post>
-                </>
-              ))}
-        </PostWrapper>
-        <ArchiveWrapper dimensions={dimensions}>
-          {posts &&
-            posts
-              .filter(
-                (data) =>
-                  data.category === 5 ||
-                  data.category === 6 ||
-                  data.category === 7
-              )
-              .slice(0, 6)
-              .map((post) => {
-                return (
-                  <HomeArchive
-                    key={post.pk}
-                    title={post.title}
-                    id={post.pk}
-                    body={post.text}
-                    category={post.category}
-                    thumbnailUrl={
-                      post.photos.length ? post.photos[0].photo : null
-                    }
-                    link={post.link}
-                  ></HomeArchive>
-                );
-              })}
-        </ArchiveWrapper>
-      </div>
-    );
-  }
+
+  return (
+    <div className="contentcontainer">
+      {showModal && (
+        <Modal
+          visible={showModal}
+          width="fit-content"
+          height="fit-content"
+          effect="fadeInDown"
+          onClickAway={() => handleClose()}
+        >
+          <div className="welcome_modal">
+            <p className="paragraph">
+              홍익시디 웹사이트가 오픈되었습니다.{" "}
+              <span role="img" aria-label="emoji">
+                🎊
+              </span>
+              <br></br>
+              <br></br>
+              상단 메뉴바의 로그인 버튼을 통해 학번으로 로그인해 주세요. 로그인
+              후 학과사무실에서 승인이 완료되면 전체 게시물을 열람 가능합니다.
+              <br></br>
+              모바일 브라우저에서 로그아웃이 제대로 안될 경우에는 브라우저
+              쿠키를 삭제 후 재시도 해 주세요.<br></br>
+              <br></br>
+              기타 다른 문제가 있을 경우에는 yinyang.fig@gmail.com으로 연락
+              바랍니다.
+            </p>
+          </div>
+        </Modal>
+      )}
+      <PostWrapper dimensions={dimensions}>
+        {posts &&
+          posts
+            .filter(
+              (data) =>
+                data.category === 1 ||
+                data.category === 2 ||
+                data.category === 3 ||
+                data.category === 4
+            )
+            .map((post) => (
+              <>
+                <LogoImage postCount={postCount}></LogoImage>
+                <Post
+                  key={post.pk}
+                  title={post.title}
+                  date={post.updated}
+                  category={post.category}
+                  id={post.pk}
+                ></Post>
+              </>
+            ))}
+      </PostWrapper>
+      <ArchiveWrapper dimensions={dimensions}>
+        {posts &&
+          posts
+            .filter(
+              (data) =>
+                data.category === 5 ||
+                data.category === 6 ||
+                data.category === 7
+            )
+            .slice(0, 6)
+            .map((post) => {
+              return (
+                <HomeArchive
+                  key={post.pk}
+                  title={post.title}
+                  id={post.pk}
+                  body={post.text}
+                  category={post.category}
+                  thumbnailUrl={
+                    post.photos.length ? post.photos[0].photo : null
+                  }
+                  link={post.link}
+                ></HomeArchive>
+              );
+            })}
+      </ArchiveWrapper>
+    </div>
+  );
 }
 
 export default ContentContainer;
