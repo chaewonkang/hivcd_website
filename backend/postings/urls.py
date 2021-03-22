5249f5b42fc61841d45c02f6336493b3b3bb09c9
seogyugim <asap0208@gmail.com>
2021-03-22 04:02:17 +0000

    m

--- backend/postings/urls.py
+++ backend/postings/urls.py
@@ -6,6 +6,10 @@ app_name = "postings"
 
 router = SimpleRouter()
 router.register(r'', views.PostViewSet)
+router.register(r'board', views.BoardViewSet)
+router.register(r'exhibition', views.ExhibitionViewSet)
+router.register(r'announce', views.AnnounceViewSet)
+router.register(r'archive', views.ArchiveViewSet)
+router.register(r'comment', views.CommentViewSet)
 
 urlpatterns = router.urls
-
