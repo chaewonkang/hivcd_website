f15204388b0ae26e1eb77dc9e3261fc35f51ec9a
seogyugim <asap0208@gmail.com>
2021-03-29 11:51:17 +0900

    --no-edit

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -2,10 +2,42 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, suser_id, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        if not suser_id:
+            raise ValueError('The given username must be set')
+        suser_id = self.model.normalize_username(suser_id)
+        user = self.model(username=suser_id, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, suser_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(suser_id, password, **extra_fields)
+
+    def create_superuser(self, suser_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_sidi', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(suser_id, password, **extra_fields)
+
+
 class Account(AbstractBaseUser):
     suser_id = models.CharField(max_length=255, blank=False, default="", unique=True)
     is_sidi = models.BooleanField(default=False)
 
     USERNAME_FIELD = "suser_id"
 
-    objects = BaseUserManager()
+    objects = UserManager()
