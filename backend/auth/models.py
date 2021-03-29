7b5f1eee5f4b14ae5d0d14b36ef59058fbfac9ad
seogyugim <asap0208@gmail.com>
2021-03-29 11:42:05 +0900

    --no-edit

--- backend/auth/models.py
+++ backend/auth/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 
 
-class Account(AbstractUser):
+class Account(AbstractBaseUser):
     suser_id = models.CharField(max_length=255, blank=False, default="", unique=True)
     is_sidi = models.BooleanField(default=False)
 
