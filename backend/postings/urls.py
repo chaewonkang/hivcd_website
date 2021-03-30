2d527df835f4a0f3a26e251e0619cf8f45b455af
seogyugim <asap0208@gmail.com>
2021-03-30 09:20:48 +0000

    --no-edit

--- backend/postings/urls.py
+++ backend/postings/urls.py
@@ -13,6 +13,6 @@ router.register(r'exhibition', views.ExhibitionViewSet)
 router.register(r'announce', views.AnnounceViewSet)
 router.register(r'archive', views.ArchiveViewSet)
 
-comment_router = NestedSimpleRouter(router, r'', base_name='posting-comments')
+comment_router = NestedSimpleRouter(router, r'', basename='posting-comments')
 
 urlpatterns = router.urls + comment_router.urls
