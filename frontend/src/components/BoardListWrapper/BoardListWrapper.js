37a20cd2a443ab9717c2b5650e653d976ac62ccc
Chaewon Kang <ilpermet@gmail.com>
2021-04-07 13:55:55 +0900

    m

--- frontend/src/components/BoardListWrapper/BoardListWrapper.js
+++ frontend/src/components/BoardListWrapper/BoardListWrapper.js
@@ -81,9 +81,9 @@ function BoardListWrapper({ list, postId, curLoc }) {
           {curLoc.includes("announce")
             ? currentsPosts.map((el, key) => {
                 let categoryName = null;
-                if (el.category === 9) categoryName = "";
-                else if (el.category === 10) categoryName = "학사정보";
-                else if (el.category === 11) categoryName = "학사내규";
+                if (el.category === 9) categoryName = "생활";
+                else if (el.category === 10) categoryName = "정보";
+                else if (el.category === 11) categoryName = "학사";
                 return (
                   <React.Fragment key={key}>
                     <div
