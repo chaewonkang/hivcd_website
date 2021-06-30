894ade80196a2cc8f0cb0baa398bb7675282a174
Chaewon Kang <saturday.the17th@gmail.com>
2021-06-30 13:06:23 +0900

    trial: cors error

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -145,6 +145,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://3.16.38.220",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 
