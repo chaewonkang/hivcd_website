2d2490a27cf11b04dd52c5ee0202c93521330cb0
nough <ndeavor5@gmail.com>
2021-09-21 10:44:30 +0900

    HotFix

--- backend/postings/models.py
+++ backend/postings/models.py
@@ -80,7 +80,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(editable=True)
     updated_at = models.DateTimeField(auto_now=True)
     text = models.TextField()
-    more_text = RichTextUploadingField()
+    more_text = RichTextUploadingField(default="", blank=True, null=True)
     link = models.URLField(default="", blank=True)
     sidi_only = models.BooleanField(default=False)
 
