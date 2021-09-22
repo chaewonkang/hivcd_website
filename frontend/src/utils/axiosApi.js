e79f6381a13b6a69b1f2edbeb5a13102b21218f4
nough <ndeavor5@gmail.com>
2021-09-22 11:30:42 +0900

    HotFix

--- frontend/src/utils/axiosApi.js
+++ frontend/src/utils/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://13.125.84.10:8000/api/v1/',
+  baseURL: 'https://15.165.47.162:8000/api/v1/',
   timeout: 5000,
 });
 
